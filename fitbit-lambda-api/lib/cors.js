20e8d619aed2e9227e7492a390799cfaa1ddb6f5
obuchel <obuchel@gmail.com>
2025-11-18 10:32:55 -0500

    Updates

--- fitbit-lambda-api/lib/cors.js
+++
@@ -1,7 +0,0 @@
-const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-  'Access-Control-Allow-Methods': 'GET,OPTIONS,POST,PUT,DELETE'
-};
-
-module.exports = { corsHeaders };
